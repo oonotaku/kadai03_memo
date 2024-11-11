4b1871f15ed53034002e75882038a24bee18f8f4
oonotaku <taku_oono@node-bee.com>
2024-11-11 19:37:13 +0900

    カメラ起動

--- js/main.js
+++ js/main.js
@@ -86,6 +86,19 @@ function saveLocation(position) {
     alert('現在地と情報を保存しました。');
 }
 
+        document.getElementById('img').addEventListener('change', function(event) {
+            const file = event.target.files[0];
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = function(e) {
+                    const preview = document.getElementById('preview');
+                    preview.src = e.target.result;
+                    preview.style.display = 'block'; // 画像を表示
+                };
+                reader.readAsDataURL(file);
+            }
+        });
+
 // 画像データをBase64形式に変換する関数
 function getImageBase64(file) {
     return new Promise((resolve, reject) => {
